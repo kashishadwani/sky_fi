c96eed284b2835febb2d7b8be3e198a89fcef347
kashishadwani <kashishadwani17@gmail.com>
2024-08-29 11:40:31 +0530

    improvised

--- app/src/main/java/com/example/sky_fi/MainActivity.kt
+++ app/src/main/java/com/example/sky_fi/MainActivity.kt
@@ -6,26 +6,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModelProvider
 import com.example.sky_fi.ui.theme.Sky_fiTheme
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val weatherViewModel = ViewModelProvider(this)[WeatherViewModel::class.java]
 
-        enableEdgeToEdge()
+        
         setContent {
             Sky_fiTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) {
+                Surface(modifier = Modifier.fillMaxWidth(),
+                    color = MaterialTheme.colorScheme.background) {
                     WeatherPage(viewModel = weatherViewModel)
                 }
             }
